23fdce06cd2b287c972b5c06fe2ff4ae52791df4
Matt Burke <git@mattburkedev.com>
2025-05-07 14:28:29 -0500

    Configure settings

--- AmplifiMetrics/AmplifiClient.cs
+++ AmplifiMetrics/AmplifiClient.cs
@@ -3,9 +3,15 @@ namespace AmplifiMetrics;
 public class AmplifiClient
 {
     private readonly HttpClient _client;
-    public AmplifiClient(HttpClient client)
+    private readonly string _defaultGateway;
+
+    public AmplifiClient(HttpClient client, string defaultGateway)
     {
+        ArgumentNullException.ThrowIfNull(client);
+        ArgumentException.ThrowIfNullOrWhiteSpace(defaultGateway);
+        
         _client = client;
+        _defaultGateway = defaultGateway;
     }
 }
 
--- AmplifiMetrics/Program.cs
+++ AmplifiMetrics/Program.cs
@@ -5,8 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services
-    .AddHttpClient<AmplifiClient>()
-    .AddHttpMessageHandler(() => new AmplifiAuthHandler(builder.Configuration["AmplifyMetricsRouterPassword"]!));
+    .AddHttpClient<AmplifiClient, AmplifiClient>(client => new AmplifiClient(client, builder.Configuration["AmplifiMetricsDefaultGateway"]!))
+    .AddHttpMessageHandler(() => new AmplifiAuthHandler(builder.Configuration["AmplifiMetricsRouterPassword"]!));
 
 var app = builder.Build();
 
