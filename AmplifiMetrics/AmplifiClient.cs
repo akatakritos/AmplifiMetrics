ccb14d22cb8363b5045ac511aa1043936ace18b3
Matt Burke <git@mattburkedev.com>
2025-05-07 15:47:03 -0500

    Query metrics from amplifi router

--- AmplifiMetrics/AmplifiClient.cs
+++ AmplifiMetrics/AmplifiClient.cs
@@ -1,31 +1,76 @@
+using System.Diagnostics;
+using System.Net;
+using System.Text.RegularExpressions;
+using AngleSharp.Html.Parser;
+
 namespace AmplifiMetrics;
 
-public class AmplifiClient
+public partial class AmplifiClient
 {
     private readonly HttpClient _client;
-    private readonly string _defaultGateway;
+    private readonly string _routerPassword;
+    private readonly CookieContainer _cookieContainer;
+    private static string? _cachedInfoToken;
 
-    public AmplifiClient(HttpClient client, string defaultGateway)
+    public AmplifiClient(HttpClient client, string routerPassword, CookieContainer cookieContainer)
     {
-        ArgumentNullException.ThrowIfNull(client);
-        ArgumentException.ThrowIfNullOrWhiteSpace(defaultGateway);
-        
         _client = client;
-        _defaultGateway = defaultGateway;
-    }
-}
-
-public class AmplifiAuthHandler : DelegatingHandler
-{
-    private readonly string _password;
-    public AmplifiAuthHandler(string password)
-    {
-        ArgumentException.ThrowIfNullOrWhiteSpace(password);
-        _password = password;
+        _routerPassword = routerPassword;
+        _cookieContainer = cookieContainer;
     }
 
-    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    public async Task<string> GetMetrics()
     {
-        return base.SendAsync(request, cancellationToken);
+        await EnsureLogin();
+
+        if (_cachedInfoToken is null)
+        {
+            var html = await _client.GetStringAsync("/info.php");
+            if (TokenRegex.Match(html) is { Success: true } match)
+            {
+                _cachedInfoToken = match.Groups[1].Value;
+            }
+            else
+            {
+                throw new Exception("Could not find token");
+            }
+        }
+
+        Debug.Assert(_cachedInfoToken is not null);
+        var response = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Post, "/info-async.php")
+        {
+            Content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "token", _cachedInfoToken! },
+                { "do", "full"}
+            })
+        });
+
+        return await response.Content.ReadAsStringAsync();
+    }
+    
+    [GeneratedRegex("var token='([0-9a-f]+)'")]
+    private static partial Regex TokenRegex { get; }
+
+    private async Task EnsureLogin()
+    {
+        if (_cookieContainer.Count > 0) return;
+        
+        var html = await _client.GetStringAsync("/login.php");
+        var parser = new HtmlParser();
+        var document = parser.ParseDocument(html);
+        var token = document.QuerySelector("input[name=token]")?.GetAttribute("value");
+        Debug.Assert(token is not null);
+        
+        // should set cookie
+        var loginResponse = await _client.PostAsync("/login.php", new FormUrlEncodedContent(new Dictionary<string, string>
+        {
+            {"token", token},
+            {"password", _routerPassword}
+        }));
+        
+        loginResponse.EnsureSuccessStatusCode();
+        Debug.Assert(_cookieContainer.Count == 1);
+
     }
 }
--- AmplifiMetrics/Program.cs
+++ AmplifiMetrics/Program.cs
@@ -1,12 +1,24 @@
+using System.Net;
 using AmplifiMetrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+var cookieContainer = new CookieContainer();
 builder.Services
-    .AddHttpClient<AmplifiClient, AmplifiClient>(client => new AmplifiClient(client, builder.Configuration["AmplifiMetricsDefaultGateway"]!))
-    .AddHttpMessageHandler(() => new AmplifiAuthHandler(builder.Configuration["AmplifiMetricsRouterPassword"]!));
+    .AddHttpClient<AmplifiClient, AmplifiClient>(client =>
+        new AmplifiClient(client, builder.Configuration["AmplifiMetricsRouterPassword"]!, cookieContainer))
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler()
+    {
+        CookieContainer = cookieContainer,
+    })
+    .ConfigureHttpClient(client =>
+    {
+        client.BaseAddress = new Uri("http://" + builder.Configuration["AmplifiMetricsDefaultGateway"]);
+    });
+
 
 var app = builder.Build();
 
@@ -15,10 +27,10 @@ if (app.Environment.IsDevelopment())
 {
 }
 
-app.MapGet("/metrics", (AmplifiClient client) =>
+app.MapGet("/metrics", async (AmplifiClient client) =>
 {
     ArgumentNullException.ThrowIfNull(client);
-    return "Hello World!";
+    return await client.GetMetrics();
 });
 
 app.Run();
